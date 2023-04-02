83fe12b065dfde2c6ea00d980b4ae0d35f6a217d
Diana <dianadianadiana586@gmail.com>
2023-04-02 16:23:14 +0600

    корзина почти готова

--- src/components/Products/ProductCard/ProductCard.jsx
+++ src/components/Products/ProductCard/ProductCard.jsx
@@ -6,9 +6,13 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { useProduct } from "../../../contexts/ProductContextProvider";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import { IconButton } from "@mui/material";
+import { useCart } from "../../../contexts/CartContextProvider";
 
 export default function ProductCard({ product }) {
   const { deleteProduct } = useProduct();
+  const { addProductToCart, checkProductInCart } = useCart();
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
@@ -26,6 +30,11 @@ export default function ProductCard({ product }) {
       </CardContent>
       <CardActions>
         <Button size="small">Details</Button>
+        <IconButton onClick={() => addProductToCart(product)}>
+          <AddShoppingCartIcon
+            color={checkProductInCart(product.id) ? "primary" : ""}
+          />
+        </IconButton>
         <Button size="small">Edit</Button>
         <Button onClick={() => deleteProduct(product.id)} size="small">
           Delete
