cb0ed2480bf08f1d00e0f21c081fd401969c520e
Diana <dianadianadiana586@gmail.com>
2023-04-04 14:25:56 +0600

    sign in, sign up done

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -2,6 +2,7 @@ import { Card } from "@mui/material";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import AboutUs from "../components/AboutUs/AboutUs";
+import Auth from "../components/Auth/Auth";
 import Cart from "../components/Cart/Cart";
 import Contacts from "../components/Contacts/Contacts";
 import FormOrder from "../components/FormOrder/FormOrder";
@@ -11,6 +12,7 @@ import PaymentForm from "../LinkCard";
 import AdminPage from "../Pages/AdminPage/AdminPage";
 import EditProductPage from "../Pages/EditProductPage/EditProductPage";
 import HomePage from "../Pages/HomePage/HomePage";
+import NotFoundPage from "../Pages/NotFoundPage/NotFoundPage";
 import ProductDetailsPage from "../Pages/ProductDetailsPage/ProductDetailsPage";
 
 const MainRoutes = () => {
@@ -19,12 +21,14 @@ const MainRoutes = () => {
     { link: "/aboutUs", element: <AboutUs />, id: 2 },
     { link: "/contacts", element: <Contacts />, id: 3 },
     { link: "/admin-page", element: <AdminPage />, id: 4 },
-    { link: "/home", element: <HomePage />, id: 5 },
+    { link: "/", element: <HomePage />, id: 5 },
     { link: "/edit/:id", element: <EditProductPage />, id: 6 },
     { link: "/cart", element: <Cart />, id: 7 },
     { link: "/details/:id", element: <ProductDetailsPage />, id: 8 },
     { link: "/form-order", element: <FormOrder />, id: 8 },
     { link: "/card", element: <PaymentForm />, id: 9 },
+    { link: "/auth", element: <Auth />, id: 10 },
+    { link: "*", element: <NotFoundPage />, id: 11 },
   ];
   return (
     <>
