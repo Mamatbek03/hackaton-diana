f9bb1f8a40f3b85b05f1eba5e4ccf5a511a8d7a4
NURAY <bekmirovan@gmail.com>
2023-04-05 18:19:09 +0600

    adaptiveforComments

--- src/components/Products/ProductDetails/ProductDetails.jsx
+++ src/components/Products/ProductDetails/ProductDetails.jsx
@@ -10,6 +10,7 @@ import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import { AddComment } from "@mui/icons-material";
+import "../ProductDetails/ProductDetails.css";
 
 const EditProduct = () => {
   const navigate = useNavigate();
@@ -37,64 +38,83 @@ const EditProduct = () => {
 
   return (
     <Box>
-      <Card sx={{ maxWidth: 500 }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            image={productDetails.image}
-            alt="shoe"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {productDetails.name}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {productDetails.like}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {productDetails.gender}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {productDetails.color}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {productDetails.size}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {productDetails.price}$
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {productDetails.description}
-            </Typography>
-            Comments:
-            <TextField
-              onChange={(e) => {
-                setComment(e.target.value);
-              }}
-              sx={{ width: "100%", height: "40%" }}
-              label="this should have been a comment"
-              variant="outlined"
-              name="name"
-              size="small"
-              value={comment}
+      <div className="allPage">
+        <Card
+          sx={{
+            width: "300px",
+            marginLeft: "10%",
+            marginBottom: "5%",
+            display: "flex",
+          }}
+        >
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image={productDetails.image}
+              alt="shoe"
             />
-            <Button
-              onClick={() => {
-                addComment();
-                saveEditProduct(productDetails);
-                clear();
-              }}
-            >
-              add comment
-            </Button>
-            <h3> Comments:</h3>
-            {productDetails.comments?.map((comment) => (
-              <p>{comment}</p>
-            ))}
-          </CardContent>
-        </CardActionArea>
-      </Card>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {productDetails.name}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {productDetails.like} likes
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {productDetails.gender}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {productDetails.color}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {productDetails.size}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {productDetails.price}$
+              </Typography>
+              <Typography
+                sx={{ marginBottom: "10%" }}
+                variant="body2"
+                color="text.secondary"
+              >
+                {productDetails.description}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <div className="comments">
+          <h2>Add Comment:</h2>
+          <TextField
+            onChange={(e) => {
+              setComment(e.target.value);
+            }}
+            sx={{ width: "100%", height: "40%" }}
+            label="this should have been a comment"
+            variant="outlined"
+            name="name"
+            size="small"
+            value={comment}
+          />
+          <Button
+            sx={{
+              color: "black",
+              backgroundColor: "lightblue",
+            }}
+            onClick={() => {
+              addComment();
+              saveEditProduct(productDetails);
+              clear();
+            }}
+          >
+            add comment
+          </Button>
+          <h3> Comments:</h3>
+          {productDetails.comments?.map((comment) => (
+            <p>{comment}</p>
+          ))}
+        </div>
+      </div>
     </Box>
   );
 };
